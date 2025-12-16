0c0f7e3d33ac422d32a946e4f7be1ca45c4d6d05
John5Cruz <john._l@hotmail.com>
2025-12-16 14:10:09 -0600

    English comments added

--- main.c
+++ main.c
@@ -1,38 +1,46 @@
 /*
 *This code shows how to do a blink for LED on board stm32f411re
-*Codigo hecho por: Jonathan Pioquinto
-*para tecnologiasjpc
+*Made by: Jonathan Pioquinto
+*for tecnologiasjpc
 */
 
 #include <stdint.h>
 
-// Definicion de registros 
-#define RCC_AHB1ENR  *((volatile uint32_t *)0x40023830) // registro para habilitar reloj
-#define GPIOA_MODER  *((volatile uint32_t *)0x40020000)	// registro de modo de funcionamiento
-#define GPIOA_OTYPER *((volatile uint32_t *)0x40020004) // registro para tipo de salida
-#define GPIOA_ODR    *((volatile uint32_t *)0x40020014)	// registro del puerto de salida
-#define GPIOA_BSRR   *((volatile uint32_t *)0x40020018) // registro de estado de pines
+// Definition of registers - Definicion de registros 
+// enables clock - registro para habilitar reloj
+#define RCC_AHB1ENR  *((volatile uint32_t *)0x40023830)
+// mode register - registro de modo de funcionamiento
+#define GPIOA_MODER  *((volatile uint32_t *)0x40020000)
+// output type register - registro para tipo de salida
+#define GPIOA_OTYPER *((volatile uint32_t *)0x40020004) 
+// output data register - registro del puerto de salida
+#define GPIOA_ODR    *((volatile uint32_t *)0x40020014)	
+// bit set/reset register - registro de estado de pines
+#define GPIOA_BSRR   *((volatile uint32_t *)0x40020018) 
 
 // Macros basicos 
-#define BIT_SET(reg, bit)    ((reg) |= (1UL << (bit))) 	// para poner un pin a 1
-#define BIT_CLEAR(reg, bit)  ((reg) &= ~(1UL << (bit)))	// para poner un pin a 0 
-#define BIT_TOGGLE(reg, bit) ((reg) ^= (1UL << (bit)))	// para cambiar el estado de un pin
-#define BIT_READ(reg, bit)   (((reg) >> (bit)) & 1UL)		// para leer el estado de un bit
+#define BIT_SET(reg, bit)    ((reg) |= (1UL << (bit))) 	// pin to 1
+#define BIT_CLEAR(reg, bit)  ((reg) &= ~(1UL << (bit)))	// pin to 0 
+#define BIT_TOGGLE(reg, bit) ((reg) ^= (1UL << (bit)))	// change state of pin
+#define BIT_READ(reg, bit)   (((reg) >> (bit)) & 1UL)		// read state of pin
 
 int main(){
-		
-    BIT_SET(RCC_AHB1ENR, 0);	// Habilita el reloj de microcontrolador
+	
+		// Enables clock - Habilita el reloj de microcontrolador
+    BIT_SET(RCC_AHB1ENR, 0);	
     
-    // Establece PA5 como salida 
-    GPIOA_MODER &= ~(3UL << 10);  // Limpia
-    GPIOA_MODER |= (1UL << 10);   // Salida
+    // Defines PA5 as output - Establece PA5 como salida 
+    GPIOA_MODER &= ~(3UL << 10);  // Clean register - Limpia registro
+    GPIOA_MODER |= (1UL << 10);   // output mode - modo de salida
     
-    while(1){//parpadea el led conectado en la placa
+    while(1){// blinks on board LED - parpadea el led conectado en la placa
 			
-        GPIOA_BSRR = (1UL << 5);      				 // PA5 a 1
-        for(volatile int i=0; i<1000000; i++);  // retraso
+        //GPIOA_BSRR = (1UL << 5);      				// previous implementation
+				BIT_SET(GPIOA_BSRR, 5);									// PA5 to 1
+        for(volatile int i=0; i<1000000; i++);  // delay - retraso
         
-        GPIOA_BSRR = (1UL << (5+16)); 				 // PA5 a 0
-        for(volatile int i=0; i<1000000; i++);  // retraso
+        //GPIOA_BSRR = (1UL << (5+16)); 				// previous implementation
+				BIT_SET(GPIOA_BSRR, (5+16));						// PA5 to 0
+        for(volatile int i=0; i<1000000; i++);  // delay - retraso
     }
 }
