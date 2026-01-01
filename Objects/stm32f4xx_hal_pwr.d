dccbc98feea0de6f7b742693acc397c0559de49c
John5Cruz <john._l@hotmail.com>
2026-01-01 02:26:30 -0600

    Added spanish translation of comments

--- Objects/stm32f4xx_hal_pwr.d
+++
@@ -1,3 +0,0 @@
-.\objects\stm32f4xx_hal_pwr.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.17.1\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_pwr.c
-.\objects\stm32f4xx_hal_pwr.o: .\RTE\_Target_1\Pre_Include_Global.h
-.\objects\stm32f4xx_hal_pwr.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.17.1\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h
--- Objects/stm32f4xx_hal_rcc.d
+++
@@ -1,3 +0,0 @@
-.\objects\stm32f4xx_hal_rcc.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.17.1\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_rcc.c
-.\objects\stm32f4xx_hal_rcc.o: .\RTE\_Target_1\Pre_Include_Global.h
-.\objects\stm32f4xx_hal_rcc.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.17.1\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h
